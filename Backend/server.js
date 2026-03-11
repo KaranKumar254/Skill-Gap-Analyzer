e38f1b203e70eff90fb6846bbf407c41444dd0f7
Karan Kumar <230101120254@centurionuniv.edu.in>
2026-03-12 00:02:41 +0530

    Update CORS origin to production URL

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "https://skill-gap-analyzer-frontend-ecui.onrender.com/",
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
