733c70b59d00dadfa3f36d2baaf22a60c7e72793
Karan Kumar <230101120254@centurionuniv.edu.in>
2026-03-11 23:47:16 +0530

    Remove hardcoded MongoDB URI
    Removed hardcoded MongoDB URI from db.js.

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,4 @@
 import mongoose from "mongoose";
-
-const MONGO_URI = "MONGO_URI=mongodb+srv://230101120254_db_user:Karan12@karan.alv6hlx.mongodb.net/skillgap?retryWrites=true&w=majority&appName=Karan
-";
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
