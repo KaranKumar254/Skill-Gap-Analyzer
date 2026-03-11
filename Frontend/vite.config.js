cb0bb5fd6b493b9de83ec796318e4230ef2536f4
Karan Kumar <230101120254@centurionuniv.edu.in>
2026-03-11 20:05:02 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://skill-gap-analyzer-backend-3joo.onrender.com/",
+        target: "https://skill-gap-analyzer-backend-3joo.onrender.com",
         changeOrigin: true,
         secure: false,
       },
